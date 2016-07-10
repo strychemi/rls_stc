3c2d68e8b8f9cd1e0b53256000991bd6d1800be8
Andrew Baik <and.baik@gmail.com>
2016-07-10 01:10:51 -0400

    finished, weird problems with test not passing fixed

--- lib/sentence.rb
+++ lib/sentence.rb
@@ -2,8 +2,29 @@ class Sentence
 
   def initialize(filename)
     raise ArgumentError unless filename.is_a? String
-    @input_file = File.open(filename, "r")
-    @output_file = nil
+    File.open(filename, "r") do |f|
+      @lines = f.readlines
+    end
+    @count = Hash.new(0)
   end
-  
+
+  def count_sentences
+    @lines.each do |line|
+      @count[line.strip] += 1
+    end
+    @count = @count.sort_by { |k, v| -v }.to_h
+  end
+
+  def output_file
+    File.open("output", "w") do |f|
+      @count.each do |k, v|
+        f.puts "\t#{v}: #{k}"
+      end
+    end
+  end
+
 end
+
+a = Sentence.new("input")
+a.count_sentences
+a.output_file
--- spec/sentence_spec.rb
+++ spec/sentence_spec.rb
@@ -1,6 +1,8 @@
 require 'sentence'
 
 describe Sentence do
+  let(:test_file_path) { File.dirname(__FILE__) + '/fixtures/test_input' }
+  let(:object) { Sentence.new(test_file_path) }
 
   describe '#initialize' do
     it 'raises an ArgumentError with 0 arguments' do
@@ -19,10 +21,40 @@ describe Sentence do
       expect{ Sentence.new("nonexistfile.txt") }.to raise_error SystemCallError
     end
 
-    it 'raises no error if file exists within same directory' do
-      allow(File).to receive(:open).and_return(File.open("output", "w"))
-      expect{ Sentence.new("input") }.not_to raise_error
+    it 'it reads the file if file exists within same directory' do
+      expect( object.instance_variable_get(:@lines) ).to be_kind_of(Array)
     end
   end
 
+  describe '#count_sentences' do
+
+    it 'returns a hash' do
+      expect(object.count_sentences).to be_kind_of(Hash)
+    end
+
+    it '@count is populated for an appropriate input' do
+      object.count_sentences
+      actual = object.instance_variable_get(:@count)
+      expected = {"I like dogs." => 2, "I like cats." => 1}
+      expect(actual).to eq(expected)
+    end
+
+    it '@count is an empty hash for a file with no lines' do
+      empty_file_path = File.dirname(__FILE__) + '/fixtures/empty_input'
+      object = Sentence.new(empty_file_path)
+      object.count_sentences
+      actual = object.instance_variable_get(:@count)
+      expect(actual).to eq({})
+    end
+  end
+
+  describe '#output_file' do
+    it '#open is called for output file' do
+      object.count_sentences
+      file = double(File)
+      expect(File).to receive(:open).with("output", "w").and_yield(file)
+      expect(file).to receive(:puts).exactly(2).times
+      object.output_file
+    end
+  end
 end
